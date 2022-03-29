909657f0dc26cdccac472118bd250c027b082501
VictorEnache <victor22enache@gmail.com>
2022-03-29 15:59:08 +0200

    esercizio delle email bonus

--- controllo_email/assets/js/main.js
+++ controllo_email/assets/js/main.js
@@ -1,24 +1,36 @@
 const mail_list = ['victor@gmail.com', 'ginevra@gmail.com', 'giacomo@gmail.com','vincenzo@gmail.com',];
 
-const user_mail_input = prompt('Inserisci la tua email!')
+const controlla = document.getElementById('controlla')
 
-let risposta;
+controlla.addEventListener('click', 
 
-const risposta_affermativa = "L'indirizzo email inserito è corretto"
+function () {
+    const user_mail_input = document.getElementById('email').value
 
-const risposta_negativa = "L'indirizzo email inserito è errato, si prega di riprovare!"
+    let title = document.querySelector('h2')
 
-for (let i = 0; i<mail_list.length; i++) {
-    const single_mail = mail_list[i]
-    
+    let risposta;
 
-    if (user_mail_input == single_mail) {
-        risposta = risposta_affermativa
+    const risposta_affermativa = "L'indirizzo email inserito è corretto"
+
+    const risposta_negativa = "L'indirizzo email inserito è errato, si prega di riprovare!"
+
+    for (let i = 0; i<mail_list.length; i++) {
+        const single_mail = mail_list[i]
+        
+
+        if (user_mail_input == single_mail) {
+            risposta = risposta_affermativa
+        }
     }
+
+    if (risposta != risposta_affermativa) {
+        risposta = risposta_negativa;
+    }
+
+    title.innerHTML = risposta;
+    console.log(mail_list)
+    console.log(risposta)
 }
 
-if (risposta != risposta_affermativa) {
-    risposta = risposta_negativa;
-}
-console.log(mail_list)
-console.log(risposta)
+)
